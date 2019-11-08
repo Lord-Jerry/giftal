0688e287d76770680574976837cb629bee0c0152
Lord Jerry <lovinjerry004@gmail.com>
2019-11-07 17:44:04 -0800

    add server

--- base.js
+++ base.js
@@ -1,4 +1,18 @@
 const puppeteer = require('puppeteer');
+
+const http = require('http');
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
+
 (async () => {
   let links;
   let browser = await puppeteer.launch({
